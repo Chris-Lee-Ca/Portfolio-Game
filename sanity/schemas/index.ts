8d65f1a739ae66e68fb3fe9086158a7697120770
Chris <chrisleebed@gmail.com>
2024-10-21 23:16:37 -0700

    remove sanity schemas from repo

--- sanity/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import education from './education-schema'
-import experience from './experience-schema'
-import link from './links-schema'
-import project from './project-schema'
-
-export const schemaTypes = [project, link, education, experience]
